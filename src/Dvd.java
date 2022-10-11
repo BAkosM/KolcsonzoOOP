ccfee513e24e8c57fb2b3988f918044fb28fb31b
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-11 15:36:50 +0200

    Kolcsonzo

---
+++ src/Dvd.java
@@ -0,0 +1,24 @@
+public class Dvd implements Kolcsonozheto, Comparable<Dvd>  {
+    private String cim;
+    private int hossz;
+
+    public Dvd(String cim, int hossz) {
+        this.cim = cim;
+        this.hossz = hossz;
+    }
+    public int getHossz() {
+        return hossz;
+    }
+    @Override
+    public String megjelenitendoNev(){
+        return String.format("%s (%d perc)", this.cim, this.hossz);
+    } 
+    @Override
+    public int meddigKolcsonozheto(){ 
+        return 7;
+    } 
+    @Override
+    public int compareTo(Dvd masikDvd){
+        return this.hossz - masikDvd.hossz;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+    static List<Kolcsonozheto> katalogus = new ArrayList<Kolcsonozheto>();
+    static List<Dvd> dvdk = new ArrayList<Dvd>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Konyv konyv1 = new Konyv("A gyűrűs úr","R.J.J.Martinez","ABCDE-12345");
+        Dvd dvd1 = new Dvd("War Star XIV. rész - A Viszavágók Birodalom",420);
+        Dvd dvd2 = new Dvd("War Star XXX. rész - Egy remény de új",690);
+        Dvd dvd3 = new Dvd("Maorak the movie",2);
+        Ujsag ujsag1 = new Ujsag("Játszó srác",2007,23);
+
+        katalogus.add(konyv1);
+        katalogus.add(dvd1);
+        katalogus.add(dvd2);
+        katalogus.add(dvd3);
+        katalogus.add(ujsag1);
+        dvdk.add(dvd1);
+        dvdk.add(dvd2);
+        dvdk.add(dvd3);
+        System.out.println();
+
+        for (Kolcsonozheto k: katalogus){
+            System.out.println(k.megjelenitendoNev()+""+k.meddigKolcsonozheto());            
+        }
+        Collections.sort(dvdk);
+        for (Dvd d: dvdk) {
+        System.out.println(d.megjelenitendoNev()+""+d.getHossz());
+        }
     }
 }
